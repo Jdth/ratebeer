b2bb329c0ea149367e53a623684185634b900fe1
Jdth <jdth@outlook.com>
2014-01-22 21:56:06 +0200

    Exercise 4, counting average for ratings

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
 	has_many :ratings
+
+	def average_rating
+		total = 0.0
+		ratings.each do |rating|
+			total = total + rating.score
+		end
+		total/(ratings.count)	
+	end
 end
