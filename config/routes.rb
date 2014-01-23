b1cd0e6175501943c68b0f29ac7b08f2c17edd71
Jdth <jdth@outlook.com>
2014-01-23 16:43:19 +0200

    Destroy ratings

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Ratebeer::Application.routes.draw do
 
   resources :breweries
 
-	get 'ratings', to: 'ratings#index'
-	get 'ratings/new', to:'ratings#new'
-	post 'ratings', to: 'ratings#create'
-
+	resources :ratings, :only => [:index, :new, :create, :destroy]
+	
 	root 'breweries#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
