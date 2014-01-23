3678cd374bac82cdb45690b770a6f3888d12ec68
Jdth <jdth@outlook.com>
2014-01-23 13:38:52 +0200

    Exercise 8, drop-down list

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,4 +5,8 @@ class Beer < ActiveRecord::Base
 	def average_rating
 		ratings.inject(0) { |total, rating| total + (rating.score) }.to_f / ratings.count
 	end
+
+	def to_s
+		"#{name}, #{brewery.name}"
+	end
 end
