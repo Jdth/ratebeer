1a6b7e2bc943e66c759dfcc2853a63644098ec8c
Jdth <jdth@outlook.com>
2014-01-30 23:30:24 +0200

    Exercise 10, password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
 	include RatingAverage
 
 	validates :username, uniqueness: true, length: { in: 3..15 }
+	validates :password, length: {in: 4..30}
+	validates :password, format: { with: /[A-Z]/,
+    message: "must include at least one letter in uppercase" }
+	validates :password, format: { with: /[0-9]/,
+		message: "must include at least one number" }
 
 	has_many :ratings
 	has_many :beers, through: :ratings
