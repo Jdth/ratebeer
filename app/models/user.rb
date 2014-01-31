30b7b00c92b044f6a6230e8d1b0ea7063e249431
Jdth <jdth@outlook.com>
2014-02-01 00:53:23 +0200

    Exercises 7 and 8, beer clubs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
 
 	has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
+	has_many :memberships, dependent: :destroy
+	has_many :beer_clubs, through: :memberships
+
 	has_secure_password
 
 	validates :username, uniqueness: true, length: { in: 3..15 }
