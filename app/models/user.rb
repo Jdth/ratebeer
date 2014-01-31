ef7efc3493f4eb9e3403764cbeaf59677cfc744a
Jdth <jdth@outlook.com>
2014-01-31 19:46:16 +0200

    Exercises 12 and 13

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
 	include RatingAverage
 
+	has_many :ratings, dependent: :destroy
+  has_many :beers, through: :ratings
+	has_secure_password
+
 	validates :username, uniqueness: true, length: { in: 3..15 }
 	validates :password, length: {in: 4..30}
 	validates :password, format: { with: /[A-Z]/,
@@ -8,8 +12,4 @@ class User < ActiveRecord::Base
 	validates :password, format: { with: /[0-9]/,
 		message: "must include at least one number" }
 
-	has_many :ratings
-	has_many :beers, through: :ratings
-
-	has_secure_password
 end
